7c26cdc8e8a54118d70ae88bee6828b1f3266563
ori <ori@blau.co.il>
2018-06-08 21:54:56 +0300

    Cart icon component

---
+++ client/directives/cartIcon.js
@@ -0,0 +1,12 @@
+angular.module('headphonesStore').
+    directive('cartIcon', ['CartFactory', function(CartFactory){
+        return {
+            restrict: 'E',
+            replace: true,
+			scope: false,
+            templateUrl: 'pages/templates/cartIcon.html',
+			link: function(scope, element, attrs){
+				scope.$root.cartAmount = CartFactory.getCartAmount();
+			}
+		}
+	}]);
