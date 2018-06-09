58816d4a77df2f1709116006329a0995aa3b9099
ori <ori@blau.co.il>
2018-06-09 09:57:13 +0300

    Add to cart functionality

---
+++ client/directives/addToCart.js
@@ -0,0 +1,22 @@
+angular.module('headphonesStore').
+    directive('addToCart', ['CartFactory', function(CartFactory){
+        return {
+            restrict: 'E',
+            replace: true,
+            scope:{
+                addToCart: '=',
+                headphones: '='
+            },
+            templateUrl: 'pages/templates/addToCart.html',
+			link: function(scope, element, attrs){
+				var element = element;
+					scope.addItemToCart = function(event){
+						CartFactory.addCartItem(scope.headphones);
+						scope.$root.cartAmount = CartFactory.getCartAmount();
+						$(event.target).next(".successMsg").fadeIn("slow", function(){
+							$(event.target).next(".successMsg").fadeOut("slow");
+						});
+					};
+			}
+		}
+	}]);
