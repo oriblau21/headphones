0e1081f382abbb5cb5c71518b23ad4ff8c3654fd
ori <ori@blau.co.il>
2018-06-13 19:42:12 +0300

    Twitter bug fix

--- server/controllers/twitter.controller.server.js
+++ server/controllers/twitter.controller.server.js
@@ -4,21 +4,30 @@ const mongoose = require('mongoose');
 const Twit = require('twit')
 let T;
 module.exports.init = () => {
-    T = new Twit({
-        consumer_key:         process.env.CONSUMER_KEY,
-        consumer_secret:      process.env.CONSUMER_SECRET,
-        access_token:         process.env.ACCESS_TOKEN,
-        access_token_secret:  process.env.ACCESS_SECRET,
-    });
+    if (process.env.CONSUMER_KEY &&
+        process.env.CONSUMER_SECRET &&
+        process.env.ACCESS_TOKEN &&
+        process.env.ACCESS_SECRET) {
+        T = new Twit({
+            consumer_key:         process.env.CONSUMER_KEY,
+            consumer_secret:      process.env.CONSUMER_SECRET,
+            access_token:         process.env.ACCESS_TOKEN,
+            access_token_secret:  process.env.ACCESS_SECRET,
+        });
+    }
 };
 
 module.exports.postTweet = (req, res) => {
     const status = req.body.text;
-    T.post('statuses/update', { status }, function(err, data, response) {
-        if (err){
-            res.status(500).end();
-        } else {
-            res.status(200).end();
-        }
-    });
+    if (!T || !status || status === ''){
+        res.status(500).end();
+    } else {
+        T.post('statuses/update', { status }, function(err, data, response) {
+            if (err){
+                res.status(500).end();
+            } else {
+                res.status(200).end();
+            }
+        });
+    }
 };
