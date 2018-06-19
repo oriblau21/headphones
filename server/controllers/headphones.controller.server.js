c78f09ac2530337cb3d06422e32fbe810338b670
ori <ori@blau.co.il>
2018-06-19 19:36:15 +0300

    Get ready to submission

--- server/controllers/headphones.controller.server.js
+++ server/controllers/headphones.controller.server.js
@@ -56,15 +56,15 @@ module.exports.fillData = (req, res) => {
             let arr = [
                 {headphonesType: 'Bose', noiseCancelingLevel: 3, name: 'Quiet Comfort 35', price: 250, description: 'Premium product by world class brand - BOSE', image: 'QC35.jpg'},
                 {headphonesType: 'Bose', noiseCancelingLevel: 2, name: 'Quiet Comfort 25', price: 200, description: 'Premium product by world class brand - BOSE', image: 'QC25.jpg'},
-                {headphonesType: 'JBL', noiseCancelingLevel: 3, name: 'Elite v700', price: 220, description: '', image: 'ELITEV700.jpg'},
-                {headphonesType: 'AKG', noiseCancelingLevel: 1, name: 'T100SI', price: 25, description: '', image: 'T100SI.jpg'},
-                {headphonesType: 'AKG', noiseCancelingLevel: 1, name: 'K845BT', price: 125, description: '', image: 'K845BT.jpg'},
-                {headphonesType: 'AKG', noiseCancelingLevel: 2, name: 'K553 PRO', price: 150, description: '', image: 'K553PRO.png'},
+                {headphonesType: 'JBL', noiseCancelingLevel: 3, name: 'Elite v700', price: 220, description: 'The best noise canceling in the market', image: 'ELITEV700.jpg'},
+                {headphonesType: 'AKG', noiseCancelingLevel: 1, name: 'T100SI', price: 25, description: 'Used by Eminem', image: 'T100SI.jpg'},
+                {headphonesType: 'AKG', noiseCancelingLevel: 1, name: 'K845BT', price: 125, description: 'Feel the power of AKG', image: 'K845BT.jpg'},
+                {headphonesType: 'AKG', noiseCancelingLevel: 2, name: 'K553 PRO', price: 150, description: 'Clear sound and beyond', image: 'K553PRO.png'},
                 {headphonesType: 'Beats by Dre', noiseCancelingLevel: 3, name: 'Studio', price: 225, description: 'For true rappers', image: 'STUDIO.jpg'},
-                {headphonesType: 'Beats by Dre', noiseCancelingLevel: 1, name: 'Solo HD', price: 175, description: '', image: 'SOLOHS.png'},
-                {headphonesType: 'Sony', noiseCancelingLevel: 0, name: '1000XM2', price: 40, description: '', image: '1000XM2.jpg'},
-                {headphonesType: 'Bose', noiseCancelingLevel: 0, name: 'soundtrue', price: 54, description: '', image: 'soundtrue.jpg'},
-                {headphonesType: 'Sony', noiseCancelingLevel: 1, name: 'XB550AP', price: 100, description: '', image: 'XB550AP.jpg'}
+                {headphonesType: 'Beats by Dre', noiseCancelingLevel: 1, name: 'Solo HD', price: 175, description: 'A must have edition to your collection', image: 'SOLOHS.png'},
+                {headphonesType: 'Sony', noiseCancelingLevel: 0, name: '1000XM2', price: 40, description: 'Traveling, working out and basicly everything', image: '1000XM2.jpg'},
+                {headphonesType: 'Bose', noiseCancelingLevel: 0, name: 'soundtrue', price: 54, description: 'True to sound', image: 'soundtrue.jpg'},
+                {headphonesType: 'Sony', noiseCancelingLevel: 1, name: 'XB550AP', price: 100, description: 'The Mercedes of headphones', image: 'XB550AP.jpg'}
             ];
 
             Headphones.collection.insertMany(arr).then((d)=> {
